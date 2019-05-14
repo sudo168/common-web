16b6937315283e8385667e719ab2434e4c3e6584
2499325873@qq.com <aaabbb123>
2019-05-14 23:35:59 +0800

    commit:

--- src/main/java/net/ewant/common/util/SpringUtil.java
+++ src/main/java/net/ewant/common/util/SpringUtil.java
@@ -10,9 +10,7 @@ public class SpringUtil implements ApplicationContextAware {
     private static ApplicationContext applicationContext;
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        if(SpringUtil.applicationContext == null){
-            SpringUtil.applicationContext = applicationContext;
-        }
+        SpringUtil.applicationContext = applicationContext;
     }
 
     public static ApplicationContext getApplicationContext() {
